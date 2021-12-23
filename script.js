0a03890c37eafb3db6af7eb7f6fa3fbc5fd4e1a2
MiDo1806 <81757415+MiDo1806@users.noreply.github.com>
2021-12-23 08:52:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// const colors = ["Yellow", "Blue", "Red", "Orange"];
+// let x = 0;
+
+// while (x < colors.length) {
+//    console.log(colors[x]);
+//    x++;
+
+//}
+
+// const colors = ["Yellow", "Blue", "Red", "Orange"];
+
+// for (let i = 0; i < colors.length; i++) {
+//    console.log(colors[i]);
+
+// }
+
+const colors = ["Yellow", "Blue", "Red", "Orange"];
+
+colors.forEach(element => console.log(element));
+
